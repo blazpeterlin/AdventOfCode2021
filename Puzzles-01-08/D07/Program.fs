3560a0ec82d3390fc5d6a759968fd320f6728861
Blaž Peterlin <blasius.peterlin@gmail.com>
2021-12-06 17:52:15 +0100

    Day 6

--- Puzzles-01-08/D07/Program.fs
+++ Puzzles-01-08/D07/Program.fs
@@ -20,24 +20,11 @@ let main argv =
     
     let mutable lns = 
         C.readLines inputFile
-        |> List.map (fun ln -> ln |>C.splitCh "," |> List.map int64)
-        |> List.concat
+        |> List.map (fun ln -> ln)
 
     0
-        
-    let target = if env=T then 256 else 256
 
-    let numFishes0 = [0..8] |> Seq.map (fun x -> (lns |> Seq.filter( fun y -> y=x) |> Seq.length |> int64)) |> List.ofSeq
 
-    let step (r1 : int64 list) _ =
-        let r2 = [0L;0L;0L;0L;0L;0L;r1[0];0L;r1[0]]
-
-        let r = List.zip (List.skip 1 (r1@[0L])) r2 |> List.map (fun(x,y) -> x+y)
-        r
-    
-    let res1 = [1..80] |> Seq.fold step numFishes0 |> List.sum
-
-    let res2 = [1..256] |> Seq.fold step numFishes0 |> List.sum
 
     0
     System.Console.ReadKey() |> ignore
