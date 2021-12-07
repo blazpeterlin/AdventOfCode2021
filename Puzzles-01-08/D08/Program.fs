a37badd6effeb88a98b2eaf1503015dc1619cadd
Blaž Peterlin <blasius.peterlin@gmail.com>
2021-12-07 06:37:24 +0100

    Day 7

--- Puzzles-01-08/D08/Program.fs
+++ Puzzles-01-08/D08/Program.fs
@@ -18,29 +18,16 @@ let main argv =
     let env =P
     let inputFile = env |> function | T -> "test.txt" | P -> "input.txt"
     
-    let mutable lns = 
+    let lns = 
         C.readLines inputFile
-        |> List.map (fun ln -> ln |> C.splitCh "," |> List.map int64)
-        |> List.concat
-
-    let mn = lns |> List.min
-    let mx = lns |> List.max
-
-    let cost a b =
-        abs(a-b)*(abs(a-b)+1L)/2L
-       
-       // 1, 3, 6, 10, 15
-       // 
-
-    let (pos1,res1) = [mn..mx] |> List.map (fun i -> (i, lns |> List.map(fun x -> abs(x-i)) |> List.sum)) |> List.minBy snd
-    
-    let (pos2,res2) = [mn..mx] |> List.map (fun i -> (i, lns |> List.map(fun x -> (cost x i))  |> List.sum)) |> List.minBy snd
+        |> List.map (fun ln -> ln)
 
     0
 
 
 
     0
+
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
 
