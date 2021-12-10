47032ec89a5d44aa1dc2b69c28eb2d76e0b779b0
Blaž Peterlin <blasius.peterlin@gmail.com>
2021-12-10 06:34:14 +0100

    Day 10

--- Puzzles-09-16/D10/Program.fs
+++ Puzzles-09-16/D10/Program.fs
@@ -13,6 +13,11 @@ let internal (+...) (x0,y0,z0) (x1,y1,z1) = (x0+x1,y0+y1,z0+z1)
 
 // type State = { EL:int; }
 
+//type BLOCK_DUMB = START | SQRL | SQRR | CRLL | CRLR | PRNL | PRNR | SGNL | SGNR | ERR of BLOCK_DUMB
+
+type BRACES = SQUARE | CURLY | ROUND | ANGLED
+type BLOCK = START | LEFT of BRACES | RIGHT of BRACES | ERR of BLOCK
+
 [<EntryPoint>]
 let main argv =    
     let env = P
@@ -20,12 +25,82 @@ let main argv =
     
     let lns = 
         C.readLines inputFile
-        |> List.map (fun ln -> ln)
+        |> List.map (fun (ln:string) -> 
+                ln.ToCharArray() 
+                |> Array.map (function 
+                    | '[' -> LEFT(SQUARE) | ']' -> RIGHT(SQUARE) 
+                    | '(' -> LEFT(ROUND) | ')' -> RIGHT(ROUND) 
+                    | '{' -> LEFT(CURLY) | '}' -> RIGHT(CURLY) 
+                    | '<' -> LEFT(ANGLED) | '>' -> RIGHT(ANGLED) | _ -> failwith "unknown")
+                |> List.ofArray
+        )
 
+    let unwrapStep (ln : BLOCK list, st : BLOCK list) =
+        let st0::stt = st
+        if (st0 |> function | ERR(_) -> true | _ -> false) then None else
 
-    0
+        match ln with
+        | [] -> None
+        | ln0::lnt -> 
+            let stNext = 
+                match st0, ln0 with
+                | LEFT(brace1), RIGHT(brace2) when brace1=brace2 -> stt
+                | LEFT(brace1), RIGHT(brace2) when brace1<>brace2 -> ERR(ln0) :: st0 :: stt
+                | _ -> ln0 :: st0 :: stt
 
+            let r = (lnt, stNext)
 
+            Some(r,r)
+
+    let unwrap (ln : BLOCK list) =
+        let st0 : BLOCK list = [START]
+        let r = (ln, st0) |> List.unfold unwrapStep |> List.last |> snd
+        r
+        
+
+    let res1 = 
+        lns
+        |> List.map unwrap
+        |> List.filter (fun (st0::_stt) -> 
+            match st0 with
+            | ERR(_) -> true
+            | _ -> false
+            )
+        |> List.map (fun (st0::_stt) -> 
+            match st0 with
+            | ERR(RIGHT(ROUND)) -> 3
+            | ERR(RIGHT(SQUARE)) -> 57
+            | ERR(RIGHT(CURLY)) -> 1197
+            | ERR(RIGHT(ANGLED)) -> 25137
+            | _ -> failwith "huh"
+            )
+        |> List.sum
+
+    let res2 =
+        lns
+        |> List.map unwrap
+        |> List.filter (fun (st0::_stt) -> 
+            match st0 with
+            | ERR(_) -> false
+            | _ -> true
+            )
+        //|> List.concat
+        |> List.map (
+            fun blcks ->
+                let r = 
+                    blcks
+                    |> List.filter (fun x -> x<>START)
+                    |> List.map (function 
+                        | LEFT(ROUND) -> 1L 
+                        | LEFT(SQUARE) -> 2L 
+                        | LEFT(CURLY) -> 3L 
+                        | LEFT(ANGLED) -> 4L 
+                        | _ ->failwith "huh")
+                    |> List.fold (fun x y -> x*5L+y) 0L
+                r
+            )
+        |> List.sort
+        |> (fun scores -> scores[scores.Length/2])
 
     0
 
